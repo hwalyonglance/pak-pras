c6d4052e2fe9493891ec22c5dbbca21590846b68
Rusman Wahab <hwalyonglance@gmail.com>
2017-08-28 22:02:45 +0700

    ...

--- application/controllers/Pdf.php
+++ application/controllers/Pdf.php
@@ -9,22 +9,32 @@ class Pdf extends CI_Controller {
 		require_once APPPATH.'/third_party/mpdf/mpdf.php';
 		$this->pdf =  new mPDF("en-GB-x", "A4", "", "", 10,10,10,10, "6", "3", "P");
 	}
-	function murid($from,$to){
+	function murid($from_or_id,$to=''){
 		error_reporting(1);
-		$gg = $this->db->select('id,nama,asal')->where('created_at >=',$from.'-01-01 00:00:00')
-				->where('created_at <=', $to.'-01-01 00:00:00')->get('f1')->result_array();
-		$i = 1;
-		mkdir('reports/murid/'.$from.'-'.$to);
-		foreach ($gg as $key => $value) {
-			$this->create([
-				'data' => [
-					'display' => 'detail',
-					'murid' => $this->M_Murid->get($value['id'])
-				],
-			], $i, $from, $to, $value['nama'], $value['asal']);
-			$i++;
+		if ($to === '') {
+			$this->pdf->WriteHTML($this->load->view('_atas',[],TRUE));
+			$this->pdf->WriteHTML($this->load->view('table/murid/index', ['data' => [
+				'display' => 'detail',
+				'murid' => $this->M_Murid->get($from_or_id)
+			]], TRUE));
+			$this->pdf->output('reports/murid/'.$from_or_id.'.pdf');
+			redirect('reports/murid/'.$from_or_id.'.pdf');
+		}else{
+			$gg = $this->db->select('id,nama,asal')->where('created_at >=',$from_or_id.'-01-01 00:00:00')
+					->where('created_at <=', $to.'-01-01 00:00:00')->get('f1')->result_array();
+			$i = 1;
+			mkdir('reports/murid/'.$from_or_id.'-'.$to);
+			foreach ($gg as $key => $value) {
+				$this->create([
+					'data' => [
+						'display' => 'detail',
+						'murid' => $this->M_Murid->get($value['id'])
+					],
+				], $i, $from_or_id, $to, $value['nama'], $value['asal']);
+				$i++;
+			}
+			redirect('reports/murid/'.$from_or_id.'-'.$to);
 		}
-		redirect('reports/murid/'.$from.'-'.$to);
 	}
 	private function create($data, $i, $from, $to, $nama, $asal){
 		$this->pdf =  new mPDF("en-GB-x", "A4", "", "", 10,10,10,10, "6", "3", "P");	
